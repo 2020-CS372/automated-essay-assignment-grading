5e296cea49dd651a90f4db982131f00beab5ba73
Jooyon Kim <gina611.kaist@gmail.com>
2020-05-02 14:19:07 +0900

    Add settings file

---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+QUALITY_DATA_DIR = os.path.join(BASE_DIR, 'data', 'quality_data')
+PLAGIARISM_DATA_DIR = os.path.join(BASE_DIR, 'data', 'plagiarism_data')
