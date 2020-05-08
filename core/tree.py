35bcbf23b58953de02cd62432b10c21962f5bdfa
Khinenw <deu07115@gmail.com>
2020-05-08 16:15:00 +0900

    (WIP) Changed ViterbiParser with CoreNLP Parser

--- core/tree.py
+++ core/tree.py
@@ -1,55 +1,41 @@
-from nltk import induce_pcfg, pos_tag
-from nltk.corpus import treebank
-from nltk.grammar import Nonterminal, Production
-from nltk.parse import ViterbiParser
-from nltk.tokenize import sent_tokenize, word_tokenize
-from tqdm.autonotebook import tqdm
+from csv import DictReader
+from nltk.parse.corenlp import CoreNLPServer, CoreNLPParser
 
 
 class TreeParser:
     def __init__(self):
         self.parser = None
-        self.productions = []
 
-        for item in tqdm(treebank.fileids()):
-            for tree in treebank.parsed_sents(item):
-                tree.collapse_unary(collapsePOS = False)
-                tree.chomsky_normal_form(horzMarkov = 2)
-                self.productions += tree.productions()
+    def setup(self, url = None):
+        if url is None:
+            server = CoreNLPServer(
+               "./data/corenlp/stanford-corenlp-4.0.0.jar",
+               "./data/corenlp/stanford-corenlp-4.0.0-models.jar",
+            )
 
-    def setup(self, corpus):
-        sentences = sent_tokenize(corpus)
-        custom_productions = self.productions[:]
+            server.start()
 
-        for sentence in sentences:
-            tokens = word_tokenize(sentence)
-            tags = pos_tag(tokens)
-            for text, pos in tags:
-                custom_productions.append(
-                    Production(Nonterminal(pos), (text, ))
-                )
+            url = 'http://localhost:9000'
 
-        grammar = induce_pcfg(Nonterminal('S'), custom_productions)
-        self.parser = ViterbiParser(grammar)
-
+        self.parser = CoreNLPParser(url=url)
         return self.parser
 
     def parse(self, sentence):
         if not self.parser:
             raise AttributeError('parser is not set up')
 
-        return self.parser.parse(word_tokenize(sentence))
+        return self.parser.raw_parse(sentence)
 
 
 if __name__ == '__main__':
-    sentences = """
-    I love you.
-    I like you.
-    """
-
     parser = TreeParser()
-    parser.setup(sentences)
+    parser.setup('http://localhost:12366')
+    print("Loaded :D")
 
-    forest = parser.parse('I love you.')
-    for tree in forest:
-        tree.pretty_print()
+    with open('data/quality_data/quality_data.csv', encoding='utf-8') as f:
+        reader = DictReader(f)
+
+        for row in list(reader)[:1]:
+            forest = parser.parse(row['essay'])
+            for tree in forest:
+                tree.pretty_print()
