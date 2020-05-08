8671b678dfe651c022842ee73d6bb3e1c69a4185
Jooyon Kim <gina611.kaist@gmail.com>
2020-05-08 19:12:09 +0900

    [WIP] use tree parser

--- quality/semantics/ambiguous.py
+++ quality/semantics/ambiguous.py
@@ -1,5 +1,8 @@
 import os
 import sys
+
+from tqdm import tqdm
+
 sys.path.append(os.path.dirname(os.path.abspath(
     os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 )))
@@ -10,34 +13,28 @@ from core.tree import TreeParser
 import pandas as pd
 
 
-def ambiguous(corpus=None, debug=False):
-    # tokenizing sentence
-    sentences = sent_tokenize(corpus)
+def ambiguous(data, debug=False):
+    for datum in tqdm(data):
+        essay = datum['essay']
+        sentences = sent_tokenize(essay)
 
-    # setup tree parser
-    parser = TreeParser()
-    parser.setup(corpus)
+        # setup tree parser
+        parser = TreeParser()
+        parser.setup()
 
-    print('Parser')
-    print(parser.parser)
-    
-    print('Productions')
-    print(len(parser.productions))
+        print('Parser')
+        print(parser.parser)
 
-    # parse sentences
-    for sentence in sentences:
-        print(sentence)
-        res = parser.parse_sentence(sentence)
+        # parse sentences
+        for sentence in sentences:
 
-        print(list(res))
-
-        # forest = parser.parse(sentence)
-        # for tree in forest:
-        #     tree.pretty_print()
+            forest = parser.parse(sentence)
+            for tree in forest:
+                tree.pretty_print()
 
 
-if __name__ == '__main__':
-    test_corpora = pd.read_excel('data\\quality_data\\quality_data.xlsx')
-    for test_corpus in test_corpora['essay']:
-        ambiguous(test_corpus, debug=True)
-        break
+# if __name__ == '__main__':
+#     test_corpora = pd.read_excel('data\\quality_data\\quality_data.xlsx')
+#     for test_corpus in test_corpora['essay']:
+#         ambiguous(test_corpus, debug=True)
+#         break
