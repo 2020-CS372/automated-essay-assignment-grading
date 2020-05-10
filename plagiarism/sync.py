b3ff81d9da7566f8518222f33b952a51b6be0651
Jooyon Kim <gina611.kaist@gmail.com>
2020-05-10 12:57:25 +0900

    Delete unneeded files

--- plagiarism/sync.py
+++
@@ -1,2 +0,0 @@
-def plagiarism_sync():
-    pass
--- quality/sync.py
+++
@@ -1,8 +0,0 @@
-from quality.semantics.sync import semantics_sync
-from quality.syntax.sync import syntax_sync
-
-
-def quality_sync():
-    syntax_sync()
-    semantics_sync()
-
