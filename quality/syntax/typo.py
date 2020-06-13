7072e57cf26e48817486a306957cf8acd31d15ea
Jooyon Kim <gina611.kaist@gmail.com>
2020-06-13 21:54:26 +0900

    Check typos

---
+++ quality/syntax/typo.py
@@ -0,0 +1,46 @@
+import requests
+import wikipediaapi
+from nltk import word_tokenize
+from nltk.corpus import wordnet, stopwords, words
+
+
+DICTIONARY_WORDS = words.words()
+WIKI = wikipediaapi.Wikipedia('en')
+
+
+def typo(data):
+    for datum in data:
+        essay = datum['essay']
+        words_ = word_tokenize(essay)
+        words_ = [w for w in words_ if w.isalpha()]
+
+        typos = []
+        for word in words_:
+            if word.lower() in stopwords.words('english'):
+                continue
+
+            synsets = wordnet.synsets(word.lower())
+            if synsets:
+                continue
+
+            if word.lower() in DICTIONARY_WORDS:
+                continue
+
+            headers = {
+                'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+                'x-rapidapi-key': '0615cad77amsh0e9589280e02861p12ae48jsnf926abb423f7',
+            }
+            url = f'https://wordsapiv1.p.rapidapi.com/words/{word.lower()}'
+            response = requests.request('GET', url, headers=headers)
+
+            if response.status_code == 404:
+                # Checking newly coined words (e.g. cyberbullying)
+                if not WIKI.page(word).exists():
+                    typos.append(word)
+
+        essay_dict = {
+            'essay_id': datum['essay_id'],
+            'typos': typos,
+        }
+
+        print(essay_dict)
