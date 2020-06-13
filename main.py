f5454c9b8836cee2614740984acef57411a3c693
Khinenw <deu07115@gmail.com>
2020-06-13 22:45:19 +0900

    Change plagiarism reader, Let reader read as List, not Iter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import csv
-import itertools
 import os
 import sys
 
@@ -17,71 +16,78 @@ def main():
     with open(QUALITY_DATA_PATH, encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         quality_data = list(reader)
-    
-    quality_data_reader = lambda: iter(quality_data)
-    def plagiarism_data_reader():
+
+    def quality_data_reader(nitems = None):
+        if nitems is None:
+            return quality_data
+
+        return quality_data[:nitems]
+
+    def plagiarism_data_reader(nitems = None):
+        source_corpus = []
         source_items = os.listdir(PLAGIARISM_SOURCE_PATH)
+        if nitems is not None:
+            source_items = source_items[:nitems]
+
+        for source in source_items:
+            source = os.path.join(PLAGIARISM_SOURCE_PATH, source)
+            with open(source, encoding = 'utf-8') as source_file:
+                source_content = source_file.read()
+                source_corpus.append(source_content)
+
+        suspicious_corpus = []
         suspicious_items = os.listdir(PLAGIARISM_SUSPICIOUS_PATH)
-        
-        for source, suspicious in itertools.zip_longest(source_items, suspicious_items):
-            source_content = None
-            suspicious_content = None
-            
-            if source is not None:
-                source = os.path.join(PLAGIARISM_SOURCE_PATH, source)
-                with open(source, encoding = 'utf-8') as source_file:
-                    source_content = source_file.read()
-            
-            if suspicious is not None:
-                suspicious = os.path.join(PLAGIARISM_SUSPICIOUS_PATH, suspicious)
-                with open(suspicious, encoding = 'utf-8') as suspicious_file:
-                    suspicious_content = suspicious_file.read()
-            
-            yield (source_content, suspicious_content)
-    
+        if nitems is not None:
+            suspicious_items = suspicious_items[:nitems]
+
+        for suspicious in suspicious_items:
+            suspicious = os.path.join(PLAGIARISM_SUSPICIOUS_PATH, suspicious)
+            with open(suspicious, encoding = 'utf-8') as suspicious_file:
+                suspicious_content = suspicious_file.read()
+                suspicious_corpus.append(suspicious_content)
+
+        return source_corpus, suspicious_corpus
+
     all_functions = {}
     for key, plagiarism_fn in PLAGIARISM_DICT.items():
         all_functions[key] = {
             'type': 'plagiarism',
             'function': plagiarism_fn
         }
-    
+
     for key, quality_fn in QUALITY_DICT.items():
         all_functions[key] = {
             'type': 'quality',
             'function': quality_fn
         }
-    
+
     parser = argparse.ArgumentParser(description = 'Grade essay')
     parser.add_argument('coverage', choices = ('all', 'sample'))
     parser.add_argument('targets', nargs='*')
-    
+
     args = parser.parse_args()
     functions = []
-    
+
     for target in args.targets:
         if target not in all_functions:
             print("Unknown function: %s" % target)
             return
-        
+
         functions.append(all_functions[target])
-    
+
     if len(functions) == 0:
         functions = all_functions.values()
-    
+
     for function_dict in functions:
         using_corpus = None
-        
+
         if function_dict['type'] == 'plagiarism':
             using_corpus = plagiarism_data_reader
-        
+
         elif function_dict['type'] == 'quality':
             using_corpus = quality_data_reader
-        
-        corpus = using_corpus()
-        if args.coverage == 'sample':
-            corpus = itertools.islice(corpus, 10)
-        
+
+        corpus = using_corpus(10 if args.coverage == 'sample' else None)
         function_dict['function'](corpus)
 
 
