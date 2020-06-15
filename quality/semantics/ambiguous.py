516a879c7bff3d09c2856f86e2b270ffc01881d7
Fi <litmify@gmail.com>
2020-06-15 19:15:07 +0900

    Fix CRLF problem & Filter too bad sentences

--- quality/semantics/ambiguous.py
+++ quality/semantics/ambiguous.py
@@ -15,19 +15,9 @@ def ambiguous(data, debug=False):
         sentences = sent_tokenize(essay)
 
         # not sastified but it works
-        CORENLP_FOLDER_PATH = os.path.join(settings.BASE_DIR, 'data', 'corenlp')
+        CORENLP_FOLDER_PATH = os.path.join(
+            settings.BASE_DIR, 'data', 'corenlp')
         os.chdir(CORENLP_FOLDER_PATH)
-
-        sentences = [
-            'I love you.',
-            'Look at the dog with one eye.',
-            'This is the moment.',
-            'A quick brown fox jumps over a lazy dog.',
-            'I went to the bank.',
-            'Let\'s stop controlling people.',
-            'Visiting relatives can be exhausting.',
-            'He fed her cat food.'
-        ]
 
         for sentence in sentences:
             # write sentences to datafile
@@ -40,20 +30,25 @@ def ambiguous(data, debug=False):
             # getting output data
             terminal_output = subprocess.check_output(
                 'java -mx500m -cp "*" edu.stanford.nlp.parser.lexparser.LexicalizedParser -printPCFGkBest 2 edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz __test_sentence__.txt',
-                shell=True                
+                shell=True
             )
             output = terminal_output.decode('utf-8')
 
             # extracting score
             tqdm.write('\n' + sentence)
             score = []
-            score_strings = re.findall('# Parse (.*)\r\n', output)
+            score_strings = re.findall('# Parse (.*)', output)
             for idx, score_string in enumerate(score_strings):
                 score.append(float(score_string.split('with score ')[1]))
-                
+
                 if idx == 0:
                     continue
-                
-            # tqdm.write(str(score))
-                tqdm.write('Clarity: ' + str( (score[idx] - score[idx - 1]) * -1 ))
+
+                # if sentence structure is too bad
+                if ((score[idx] - score[idx - 1]) * -1) < 0.05:
+                    tqdm.write('Sentence is too bad: ' + str((score[idx] - score[idx - 1]) * -1))
+                    continue
+
+                tqdm.write('Clarity: ' +
+                           str((score[idx] - score[idx - 1]) * -1))
                 tqdm.write('\n')
